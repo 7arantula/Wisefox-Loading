d8bf6dbeec7e252c53f131907d9173c40d5a0f2a
ARYA <tarantulatoxicant@gmail.com>
2025-11-21 20:48:01 +0530

    Pollishing
    Bug fix
    Touch click
    Animation
    Ball path

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let targetStrength = 0;
 let ease = 0.05;
 let animating = false;
 let kicked = false;
+let isTrackingBall = false;
 
 let tokens = d.split(/(\s|,)/).filter(t => t.trim() !== "");
 
@@ -42,12 +43,12 @@ function animate() {
     requestAnimationFrame(animate);
 }
 
-
-
 document.addEventListener("mousedown", (e)=>{
     mousedown = true;
     const mouse = getSVGPoint(e);
-    lookAt(mouse);  
+    if(isTrackingBall==false){ 
+    lookAt(mouse);
+    } 
     targetStrength = 1;   
     if (!animating) requestAnimationFrame(animate);
 });
@@ -59,54 +60,49 @@ document.addEventListener("mouseup", (e)=>{
 });
 
 document.addEventListener("mousemove", (e) => {   
-    const mouse = getSVGPoint(e); 
-    lookAt(mouse);    
+    const mouse = getSVGPoint(e);
+    if(isTrackingBall==false){ 
+    lookAt(mouse);
+    }
 });
 
 document.addEventListener("touchstart", (e) =>{
     e.preventDefault();
+
     touchstart = true;
     targetStrength = 1; 
     const touch = e.changedTouches[0];
-    if(touch){
+    if(touch && isTrackingBall==false){
     const mouse = getSVGPoint(touch);
     lookAt(mouse);
     }
     if (!animating) requestAnimationFrame(animate);
 },{ passive: false });
 
+
 document.addEventListener("touchend", (e) =>{
-    
+
     touchstart = false;
     targetStrength = 0; 
-    const touch = e.changedTouches[0];
-    if(touch){
-    const mouse = getSVGPoint(touch);
-    lookAt(mouse);
-    }
     if (!animating) requestAnimationFrame(animate);
 });
 
 document.addEventListener("touchmove", (e) =>{
     const touch = e.changedTouches[0];
-    if(touch){
+    if(touch && isTrackingBall==false){
     const mouse = getSVGPoint(touch);
     targetStrength = 1;
     lookAt(mouse);
     }
 });
 
-
-
-
-
 const svg = document.querySelector("svg");
 const leftEye = document.getElementById("eyeLeft");
 const rightEye = document.getElementById("eyeRight");
 
 const eyes = [
-  { sclera:{cx:38.5, cy:63, rx:4.5, ry:5.5}, pupil:leftEye },
-  { sclera:{cx:63,   cy:69, rx:4.5, ry:5.5}, pupil:rightEye }
+  { sclera:{cx:38.5, cy:63, rx:5.5, ry:6.5}, pupil:leftEye },
+  { sclera:{cx:63,   cy:69, rx:5.5, ry:6.5}, pupil:rightEye }
 ];
 
 // Converting Mouse to SvG coordinates.
@@ -138,59 +134,89 @@ function lookAt(mouse)
         let px = cx + ex * (rx - pupilR);
         let py = cy + ey * (ry - pupilR);
 
-        if(mousedown==true || touchstart==true){
+        if(mousedown==true || touchstart==true || isTrackingBall==true){
             eye.pupil.setAttribute("cx", px);
             eye.pupil.setAttribute("cy", py);    
         }
     });
 }
 
-
+let canvas = document.getElementById("canvas");
 let soccer = document.getElementById("soccer");
 let kick = document.getElementById("kick");
 kick.addEventListener("click", kickBall);
+kick.addEventListener("touchend", kickBall);
 let ballSvg = document.getElementById("ballSvg");
 let blinkSvg = document.getElementById("blink");
 
+function lookAtBall(){
+    
+    const style = window.getComputedStyle(soccer);
+    const matrix = new DOMMatrix(style.transform);
+    
+    const currentX = matrix.m41;
+    const currentY = matrix.m42+300;
 
+    const mouse = { x: currentX, y: currentY };
+    lookAt(mouse);
+    
+}
+
+function trackBallLoop() {
+    if (isTrackingBall) {
+        lookAtBall();
+        requestAnimationFrame(trackBallLoop); 
+    }
+}
 
 function kickBall()
 {
     blinkSvg.style.animation = 'none';
     blinkSvg.style.strokeOpacity = '0';
     kick.style.pointerEvents= 'none'; 
+    isTrackingBall = true;
+    requestAnimationFrame(trackBallLoop);
 
-
-    console.log("KickedBall to the fox");
-    const animateOut = soccer.animate([
-        { transform: 'translate(-90px, 33px) rotate(200deg)' }
-    ], {
+        const animateOut = soccer.animate([
+            { transform: 'translate(-90px, 33px) rotate(200deg)' , composite: "replace"}
         
-        duration: 700, 
-        easing: 'ease-out',
-        iterations: 1, 
-        fill: 'forwards', 
-        
-    });
+        ], {
+            
+            duration: 700, 
+            easing: 'ease-out',
+            iterations: 1, 
+            fill: 'forwards', 
+            
+        });
 
-    animateOut.finished.then(()=>{
+        animateOut.finished.then(()=>{
+
+            soccer.style.transform = 'translate(-90px, 33px) rotate(200deg)';
+            animateOut.cancel();
+
             return new Promise(resolve => setTimeout(resolve, 1000));
+
         }).then (()=>{
             kicked = false;
             kickLeg();
 
+            const style = window.getComputedStyle(soccer);
+            const matrix = new DOMMatrix(style.transform);
+            const startX = matrix.m41;
+            const startY = matrix.m42;
 
-        const distanceX = -1000; 
-        const apexY = -700;
-        const startX = -90;
-        const startY = 33;
-        const totalRotation = -2000;
-        const endYOffset = 700;
-        const duration = 2500;
-        const keyframes = [];
-        const numKeyframes = 11;
+            const [Wx, Hy] = calculateCanvas();
 
-        for (let i = 0; i < numKeyframes; i++) {
+            const distanceX = ((Wx/2)*-1)+100; 
+            const apexY =  (Hy/2)*-1;
+            const totalRotation = -5000;
+            const endYOffset = (Hy/2)-100;
+            const duration = 2000;
+            const keyframes = [];
+            const numKeyframes = 11;
+
+
+            for (let i = 0; i < numKeyframes; i++) {
             const progress = i / (numKeyframes - 1);
 
             const verticalProgress = 4 * progress * (1 - progress);
@@ -203,28 +229,35 @@ function kickBall()
             const currentRotation = totalRotation * progress;
 
             keyframes.push({
-                transform: `translate(${currentX}px, ${currentY}px) rotate(${currentRotation}deg)`,
+                transform: `translate(${currentX}px, ${currentY}px) rotate(${currentRotation}deg)`
             });
         }
 
+        const finalTransform = keyframes[keyframes.length - 1].transform;
+
         const soccerAnimation = soccer.animate(
             keyframes , 
-            
             { 
                 duration: duration, 
                 easing: 'linear',
                 iterations: 1, 
                 fill: 'forwards',
-            }
+            });
 
-        );
             const timeBeforeEnd = 1000;
             setTimeout(() => {
-                ballSvg.style.opacity = '0';
+               ballSvg.style.opacity = '0';
             }, duration - timeBeforeEnd);
-            return soccerAnimation.finished;
 
-        }).then (()=>{
+            return soccerAnimation.finished.then (()=>{
+
+            soccer.style.transform = finalTransform;
+            soccerAnimation.cancel();
+            
+            });
+
+        }).then (()=>{ 
+
             return new Promise(resolve => setTimeout(resolve, 400));
 
         }).then (()=>{
@@ -234,12 +267,14 @@ function kickBall()
         }).then (()=>{
             return new Promise(resolve => setTimeout(resolve, 700));
 
-        }).then (()=>{    
-            soccer.getAnimations().forEach(animation => animation.cancel());
-            soccer.style.transform = 'translate(-220px, 33px)';
+        }).then (()=>{   
+             
+            isTrackingBall = false; 
             blinkSvg.style.animation = 'blinking 1.5s infinite steps(4)';
             ballSvg.style.opacity = '1';
             kick.style.pointerEvents= 'auto'; 
+
+            soccer.style.transform = 'translate(-220px, 33px)';
         })
         .catch(error => {
         console.error("An animation step failed:", error);
@@ -247,14 +282,12 @@ function kickBall()
     
 }
 
-
 let LQ = 47.1 ;
 let RQ = 53.1 ;
 
 let lxIndex = tokens.indexOf(LQ.toString());
 let lyIndex = lxIndex + 1;
 let rxIndex = lyIndex + 2;
-
 let offset = 0;
 
 function kickLeg()
@@ -268,7 +301,7 @@ function kickLeg()
     {
         offset = 10;
     }
-    console.log(lxIndex+"  "+lyIndex+"  "+rxIndex)
+    
 
     let baseLy = parseFloat(tokens[lyIndex]);
     let baseLx = parseFloat(tokens[lxIndex]);
@@ -279,4 +312,11 @@ function kickLeg()
     tokens[rxIndex] = baseRx + offset;
 
     foxBody.setAttribute("d", tokens.join(" "));
+}
+
+function calculateCanvas(){
+
+    let sizeX = canvas.offsetWidth;
+    let sizeY = canvas.offsetHeight;
+    return [sizeX,sizeY];
 }
